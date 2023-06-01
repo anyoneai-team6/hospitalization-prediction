8ba3d018b63bf47fa950ef0605e1214e36a2fbcd
AlbertoRoGlez <albertorgdev@gmail.com>
2023-06-01 00:32:04 -0600

    json data readed as a dataframe and saved as a csv

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -11,6 +12,12 @@ def predict():
     # Store form data in a JSON file
     with open('data.json', 'w') as json_file:
         json.dump(form_data, json_file)
+
+    # Convert JSON data to a Pandas dataframe
+    df = pd.DataFrame.from_dict(form_data, orient='index').transpose()
+
+    # Save the dataframe as a CSV file
+    df.to_csv(r'../data/form/form_data.csv', index=False)
 
     # Placeholder prediction
     prediction_result = 'Some prediction result'
