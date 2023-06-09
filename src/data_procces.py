b42e470886d5814f8a48a24b01e014f9e7dd2c7f
Your Name <leocabsoad@example.com>
2023-06-09 17:19:29 -0300

    commit viernes

--- src/data_procces.py
+++ src/data_procces.py
@@ -45,19 +45,6 @@ def apply_resample(X_train, y_train,v=1.35):
     return X_train_resampled, y_train_resampled
 
 
-def preprocess_no_bin(X_train, X_test, strategy="knn"):
-    if strategy == "simple":
-        si = SimpleImputer(strategy='median')
-        si.fit(X_test)
-        X_test = si.transform(X_test)
-        X_train = si.transform(X_train)
-    elif strategy == "knn":
-        knn = KNNImputer(n_neighbors=8)
-        knn.fit(X_test)
-        X_test = knn.transform(X_test)
-        X_train = knn.transform(X_train)
-
-
 def apply_standard_scaler(X_train, X_test):
     scaler = RobustScaler()
     X_train_scaled = scaler.fit_transform(X_train)
@@ -88,23 +75,3 @@ def find_name_list(l,x):
             columnas_elegibles.append(columna)
     
     return columnas_elegibles
-
-
-def find_with_null_col(df, x):
-    columnas_con_nulos = []
-    nulos_por_columna = df.isnull().sum().sort_values(ascending=False)
-    
-    for columna, cantidad in nulos_por_columna.items():
-        if cantidad > x:
-            columnas_con_nulos.append(columna)
-    
-    return columnas_con_nulos
-
-
-def fill_column(df, x, strategy='most_frequent'):
-    imputer = SimpleImputer(strategy=strategy)
-    print(df[x].value_counts())
-    print(f'nulls: {df[x].isnull().sum()}')
-    df[[x]] = imputer.fit_transform(df[[x]])  
-    
-    return df[x]
--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,11 +1,20 @@
-import numpy as np
 import pandas as pd
 
 
 def get_bin_columns(dataframe):
+    """
+    Get the binary and non-binary columns from a dataframe.
+    
+    Args:
+        dataframe (pd.DataFrame): The input dataframe.
+    
+    Returns:
+        tuple: A tuple containing two lists - binary columns and non-binary columns.
+    
+    """
     columnas_binarias = []
     columnas_no_binarias = []
-
+    
     for columna in dataframe.columns:
         valores_unicos = dataframe[columna].dropna().unique()
         
@@ -19,7 +28,17 @@ def get_bin_columns(dataframe):
 
 
 def fill_with_encoding(dataframe: pd.DataFrame, columns: list) -> pd.DataFrame:
-    # Reemplazar los 0 por -1
+    """
+    Fill specified columns in a dataframe with encoding values.
+    
+    Args:
+        dataframe (pd.DataFrame): The input dataframe.
+        columns (list): List of column names to fill.
+    
+    Returns:
+        pd.DataFrame: The modified dataframe with filled columns.
+    
+    """
     dataframe[columns] = dataframe[columns].replace(0, -1)
     
     dataframe[columns] = dataframe[columns].fillna(0)
@@ -29,6 +48,16 @@ def fill_with_encoding(dataframe: pd.DataFrame, columns: list) -> pd.DataFrame:
 
 
 def fill_with_mean(dataframe: pd.DataFrame, columns: list):
+    """
+    Fill specified columns in a dataframe with their mean values.
+    
+    Args:
+        dataframe (pd.DataFrame): The input dataframe.
+        columns (list): List of column names to fill.
+    
+    Returns:
+        pd.DataFrame: The modified dataframe with filled columns.
+    """
     for column in columns:
         mean = dataframe[column].mean()
         dataframe[column].fillna(mean, inplace=True)
@@ -38,6 +67,15 @@ def fill_with_mean(dataframe: pd.DataFrame, columns: list):
 
 
 def fast_fill(dataframe: pd.DataFrame):
+    """
+    Perform fast filling of binary and non-binary columns in a dataframe.
+    
+    Args:
+        dataframe (pd.DataFrame): The input dataframe.
+    
+    Returns:
+        pd.DataFrame: The modified dataframe after fast filling.
+    """
     binary_cols, non_binary_cols = get_bin_columns(dataframe)
     dataframe[binary_cols] = fill_with_encoding(dataframe, binary_cols)
     dataframe[non_binary_cols] = fill_with_mean(dataframe, non_binary_cols)
